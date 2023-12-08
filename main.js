853942ba1bc6ddcb71a7027a5dba68b4d7cb57a4
Felipe Haddad <felipehaddad25@gmail.com>
2023-12-07 22:11:45 -0300

    finish electron tutorial

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron/main')
 const path = require('node:path')
 
-// função que vai criar a janela e vai mostrar o código index.html
 const createWindow = () => {
-    const win = new BrowserWindow({
-        with:800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    })
-    win.loadFile('src/index.html')
+  const win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('./src/index.html')
+  // win.loadURL("http://www.google.com")
+  // https://planecards.vercel.app/
 }
 
-
 app.whenReady().then(() => {
-    createWindow()
+  ipcMain.handle('ping', () => 'testando')
+  createWindow()
 
-    // macOS, abrir janela se não tiver nenhuma aberta
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 })
 
-// fechar app quando todas as janelas forem fechadas, Windows e Linux
+
 app.on('window-all-closed', () => {
-    if (process.platform !== "darwin") app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
--- preload.js
+++ preload.js
@@ -1,10 +1,22 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
+const { contextBridge, ipcRenderer } = require('electron')
 
-    for (const dependecy of ['chrome', 'node', 'electron']) {
-        replaceText(`${dependecy}-version`, process.versions[dependecy])
-    }
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const replaceText = (selector, text) => {
+//         const element = document.getElementById(selector)
+//         if (element) element.innerText = text
+//     }
+
+//     for (const dependecy of ['chrome', 'node', 'electron']) {
+//         replaceText(`${dependecy}-version`, process.versions[dependecy])
+//     }
+// })
+
+
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
+  // we can also expose variables, not just functions
 })
